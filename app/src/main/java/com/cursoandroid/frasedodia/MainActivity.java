5f6cd86258c5e2c12b628152ff4aa981f9c6b88e
Renan Costa Alencar <renan.costaalencar@gmail.com>
2019-07-06 01:06:20 -0300

    #App2 - Frases do Dia - Final

--- app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
@@ -3,15 +3,22 @@ package com.cursoandroid.frasedodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textoNovaFrase;
     private Button botaoNovaFrase;
 
-    private String[] frases = {"teste", "teste2"};
+    private String[] frases = {
+            "É pra frente que se anda, é pra cima que se olha e é lutando que se conquista.",
+            "Ciclo da vida: tentar, cair, levantar, recomeçar. Nunca desistir.",
+            "Treine sua mente para ver o lado bom de qualquer situação."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,18 @@ public class MainActivity extends AppCompatActivity {
         textoNovaFrase = findViewById(R.id.textoNovaFraseId);
         botaoNovaFrase = findViewById(R.id.botaoNovaFraseId);
 
-        textoNovaFrase.setText(frases[0]);
+        //textoNovaFrase.setText(frases[0]);
+
+        botaoNovaFrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomico = new Random();
+                int numeroAleatorio = randomico.nextInt(frases.length);
+
+                textoNovaFrase.setText(frases[numeroAleatorio]);
+            }
+        });
 
     }
 }
