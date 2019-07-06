f9c7941cfe52fc1e512e4e03f9c53348e66a8a57
Renan Costa Alencar <renan.costaalencar@gmail.com>
2019-07-06 00:57:44 -0300

    #App2 - Frases do Dia - Entenda Arrays - Parte 1

--- app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
+++ app/src/main/java/com/cursoandroid/frasedodia/MainActivity.java
@@ -3,12 +3,25 @@ package com.cursoandroid.frasedodia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textoNovaFrase;
+    private Button botaoNovaFrase;
+
+    private String[] frases = {"teste", "teste2"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoNovaFrase = findViewById(R.id.textoNovaFraseId);
+        botaoNovaFrase = findViewById(R.id.botaoNovaFraseId);
+
+        textoNovaFrase.setText(frases[0]);
+
     }
 }
